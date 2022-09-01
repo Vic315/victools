b94615263d46f43d8618950780eea2ad9f430a9f
vic.zeng <vic.zeng@idreamsky.com>
2022-09-01 11:35:42 +0800

    [22-09-01 周四 11:35:19] commit: 隐藏ver (VICPC) v7

--- art_commit-Vic.bat
+++ art_commit-Vic.bat
@@ -3,6 +3,7 @@ set d=%date:~0,1000%
 set t=%time:~0,8%
 set timestamp=%d% %t%
  if not exist "ver.txt" echo 0 >> ver.txt
+
 set filePath=ver.txt
 set /a lastline = 0
 for /f  %%i in (%filePath%) do (
@@ -15,7 +16,9 @@ echo.
 set /p comments= 本地提交说明: 
 echo [%timestamp%] commit: %comments% (%COMPUTERNAME%) v%lastline% >> history.txt
 echo %lastLine% >> ver.txt
- 
+
+attrib +s +h ver.txt
+
 git add ver.txt
 git add history.txt
 git commit -m "[%timestamp%] commit: %comments% (%COMPUTERNAME%) v%lastline%"
